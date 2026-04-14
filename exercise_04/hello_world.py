e708cacffe99ea1cbc0b68f1b0ee8a968dc5077a
A.O.S <achyutha.os@vittindia.com>
2026-04-15 00:30:11 +0530

    added info about C++

--- exercise_04/hello_world.py
+++ exercise_04/hello_world.py
@@ -1,3 +1,10 @@
+print("""C++ is a high-performance, compiled programming language 
+known for its efficiency and control over system resources. 
+It supports multiple programming paradigms, including 
+procedural, object-oriented, and generic programming. 
+Its speed and versatility make it a popular choice for 
+system/software development and competitive programming.""")
+
 print("""Python is a high-level, interpreted programming language 
 known for its readability and versatility. It supports multiple 
 programming paradigms, including structured, object-oriented, 
