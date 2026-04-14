cfbd0e3bd24a5ae2b7c68f3d5072ac4d84a9c988
A.O.S <achyutha.os@vittindia.com>
2026-04-15 00:25:25 +0530

    adding hellowrld for ex_04

---
+++ exercise_04/hello_world.py
@@ -0,0 +1,6 @@
+print("""Python is a high-level, interpreted programming language 
+known for its readability and versatility. It supports multiple 
+programming paradigms, including structured, object-oriented, 
+and functional programming. Its large standard library and 
+active community make it one of the most popular languages 
+in the world today.""")
