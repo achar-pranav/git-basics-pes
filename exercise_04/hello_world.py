4947532f01cbfe5c5044a4024dca08454ba9c69c
A.O.S <achyutha.os@vittindia.com>
2026-04-15 00:28:18 +0530

    added more details about java

--- exercise_04/hello_world.py
+++ exercise_04/hello_world.py
@@ -12,3 +12,10 @@ programming paradigms, including structured, object-oriented,
 and functional programming. Its large standard library and 
 active community make it one of the most popular languages 
 in the world today. Moreover, Python's simplicity and ease of use make it an excellent choice for beginners and experienced developers alike.""")
+
+print("""Java is a high-level, class-based, object-oriented 
+programming language that is designed to have as few 
+implementation dependencies as possible. It is a general-purpose 
+programming language intended to let programmers write once, 
+run anywhere, meaning that compiled Java code can run on all 
+platforms that support Java without the need for recompilation.""")
