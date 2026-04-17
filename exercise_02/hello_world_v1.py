e686848280d872597e74fee56c84094edbafc741
Pranav Achar <pranavhachar@gmail.com>
2026-04-18 01:00:41 +0530

    removed old deprecated code hello world v1.py

--- exercise_02/hello_world_v1.py
+++
@@ -1,6 +0,0 @@
-print("""Python is a high-level, interpreted programming language 
-known for its readability and versatility. It supports multiple 
-programming paradigms, including structured, object-oriented, 
-and functional programming. Its large stmdard libary and 
-active community make it one of the most popular languages 
-in the world today.""")
